db76dda5472e1887d2b23609a7b09b9cfb80febe
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2020-04-04 13:33:36 -0700

    Add basic layout for landing page and input field for zipcode

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -6,8 +6,12 @@ class Layout extends Component {
   render() {
     return (
       <>
-        <div className={classes.Layout}>
-          <h1>Covidhax landing page</h1>
+        <div className={classes.layout}>
+          <h1 className={classes.header}>Covidhax landing page</h1>
+          <div className={classes.zipcode}>
+            <label for="zipcode">Zipcode:</label>
+            <input type="text" id="zipcode" value="Zipcode" name="zipcode"/>
+          </div>
         </div>
       </>
     );
