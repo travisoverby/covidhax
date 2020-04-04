70b1711cfdb3941065b2ca1a3be93a8bae81fd27
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2020-04-04 10:22:45 -0700

    Create componets folder to hold components and create layout component

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classes from './Layout.module.css';
+
+class Layout extends Component {
+
+  render() {
+    return (
+      <>
+        <div className={classes.Layout}>
+          <h1>Covidhax landing page</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Layout;
