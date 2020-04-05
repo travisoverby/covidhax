f252f851bd80c425540401a10f0559ab3d011c37
Ali Zand <ali.zand@realpage.com>
2020-04-04 22:19:53 -0700

    7 Add UUID and add charity index with test

--- client/src/components/Charity/index.js
+++ client/src/components/Charity/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { v1 } from 'uuid';
+import Alert from 'react-bootstrap/Alert';
 
-export const Charity = ({}) => {
-  return <div>Charity List</div>;
-};
+class Charity extends React.Component {
+  render() {
+    const { charities } = this.props;
+
+    if (charities !== undefined) {
+      return (
+        <ListGroup>
+          {charities.map((charity) => {
+            return <ListGroup.Item key={v1()}>{charity.name}</ListGroup.Item>
+          })}
+        </ListGroup>
+      );
+    } else {
+      return <Alert variant="secondary">There are no charities in our database!</Alert>;
+    }
+  }
+}
+
+export default Charity;
+
