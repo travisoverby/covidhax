e8c62dccc3c7fc6c89f81ce85abf714a137fab37
Ali Zand <ali.zand@realpage.com>
2020-04-04 19:20:16 -0700

    Add rewrite. import correct the correct function name. remove bable-jest and jest as they come with react

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,12 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    proxy(
-      '/api', {
-        target: 'http://localhost:3000/',
-      }
-    )
+    createProxyMiddleware('/api', {
+      target: 'http://localhost:3000/',
+      pathRewrite: {
+        '^/api': '/', // remove base path
+      },
+    })
   );
 };
