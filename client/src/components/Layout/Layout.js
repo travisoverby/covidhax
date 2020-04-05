b7a8d3e11fc7a7503e01c22097c3556d07e547ab
Ali Zand <ali.zand@realpage.com>
2020-04-04 20:22:49 -0700

    20 Add router, Create Home, and Login, refactor
    * client/src/components/Home/index.js
      Create an empty home component
    
    * client/src/components/User/login.js
      Create login component
    
    * client/package-lock.json
    * client/package.json
      Get react router dom
    
    * client/src/components/Layout/Layout.js
      Add Routes for home and login. Use Container from bootstrap
    
    * client/src/components/Layout/Layout.module.css
      Remove empty CSS class

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Layout.module.css';
 import TestWidget from '../widgets/TestWidget/TestWidget';
+import Container from 'react-bootstrap/Container';
 
-class Layout extends Component {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from '../Home';
+import { Login } from '../User/login'
+
+class Layout extends React.Component {
 
   render() {
     return (
-      <>
-        <div className={classes.layout}>
-          <h1 className={classes.header}>Covidhax landing page</h1>
-          <TestWidget/>
-          <div className={classes.zipcode}>
-            <label for="zipcode">Zipcode:</label>
-            <input type="text" id="zipcode" value="Zipcode" name="zipcode"/>
-          </div>
+      <Container fluid={true}>
+        <h1 className={classes.header}>Covidhax landing page</h1>
+
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/user/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+
+        <TestWidget/>
+        <div className={classes.zipcode}>
+          <label for="zipcode">Zipcode:</label>
+          <input type="text" id="zipcode" value="Zipcode" name="zipcode"/>
         </div>
-      </>
+      </Container>
     );
   }
 }
