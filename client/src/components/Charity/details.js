75ef416d9b263ad57e1d3712b1281c8072359322
Ali Zand <ali.zand@realpage.com>
2020-04-04 21:53:25 -0700

    7 Create details and test

--- client/src/components/Charity/details.js
+++ client/src/components/Charity/details.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class Details extends Component {
   constructor(props) {
